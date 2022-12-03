9cdf1276877f5204c41053af4ce7016b25298d6f
lamnv1992 <nguyenvanlamtrust@gmail.com>
2022-12-03 11:03:12 +0900

    Bài tập buổi 5
    1 - Khai báo lớp Person với các thuộc tính: name(String), age(int), email(String?), các phương thức: showName(), showAge().
    2 - Khai báo lơp Student kế thừa lớp person, thêm các thuộc tính: grade(int), className(String), mark(int). Phương thức showMarkPoint().
    3 - Khai báo lớp Teacher kế thừa lớp person, thêm các thuộc tính: department(String), subject(String). Phương thức: teach().
    4- Tạo constructor cho các lớp ở trên.
    Note: Các phương thức chỉ cần khai báo, k cần viết nội dung thực thi bên trong

--- dart_application_1/bin/dart_application_1.dart
+++ dart_application_1/bin/dart_application_1.dart
@@ -1,57 +1,55 @@
 import 'package:dart_application_1/dart_application_1.dart' as dart_application_1;
-
 /* 
-Bài tập 1:
-Cho 1 mảng distances = [1,3,10,4,2,6,7,8,4,2,1];
-Viết hàm:
-- Tìm giá trị lớn nhất, nhỏ nhất trong mảng, vị trí(đầu tiên nếu trùng).
-- Tính tổng giá trị các phần tử của mảng.
-Bài tập 2:
-cho map: 
-{
-‘name’: ‘Hoang’,
-‘age’: 23,
-‘job’: ‘IT’
+1 - Khai báo lớp Person với các thuộc tính: name(String), age(int), email(String?), các phương thức: showName(), showAge().
+2 - Khai báo lơp Student kế thừa lớp person, thêm các thuộc tính: grade(int), className(String), mark(int). Phương thức showMarkPoint().
+3 - Khai báo lớp Teacher kế thừa lớp person, thêm các thuộc tính: department(String), subject(String). Phương thức: teach().
+4- Tạo constructor cho các lớp ở trên.
+Note: Các phương thức chỉ cần khai báo, k cần viết nội dung thực thi bên trong
+ */
+
+class Person {
+  String name;
+  int age;
+  String? email;
+  Person({
+    required this.name,
+    required this.age,
+    String? email,
+  });
+  void showName(){}
+  void showAge(){}
+
 }
 
-- Hiển thị ra màn hình:  HOANG-23-IT.
-- Thêm thông tin cho map: ‘email’: ‘abc@gmail.com’
- */
+class Student extends Person {
+  int grade;
+  String className;
+  int mark;
+
+  Student({
+    required name,
+    required age,
+    required this.grade,
+    required this.className,
+    required this.mark,
+    String? email,
+  }) : super(name: name, age: age, email: email);
+  void showMarkPoint(){}
+}
+
+class Teacher extends Person {
+  String department;
+  String subject;
+  Teacher({
+    required super.name,
+    required super.age,
+    required this.department,
+    required this.subject,
+    String? email,
+  });
+  void teach() {}
+}
+
 void main(List<String> arguments) {
-  //print('Hello world: ${dart_application_1.calculate()}!');
-  final distances = [1,3,10,4,2,6,7,8,4,2,1];
-  var sum      = 0,
-      maxIndex = 0,
-      minIndex = 0;
-  for(int i = 0; i < distances.length; i++) {
-    if(distances[maxIndex] < distances[i]) {
-      maxIndex = i;
-    }
-    if(distances[minIndex] > distances[i]) {
-      minIndex = i;
-    }
-    sum += distances[i];
-  }
-  print('Giá trị lớn nhất: ${distances[maxIndex]} tại index: $maxIndex');
-  print('Giá trị nhỏ nhất: ${distances[minIndex]} tại index: $minIndex');
-  print('Tổng giá trị các phần tử: $sum');
-
-  ///########## Bài tập 2: #########
-  Map<String, dynamic> info = {
-  'name': 'Hoang',
-  'age': 23,
-  'job': 'IT'
-  };
-  String str = [
-    info['name'].toUpperCase(),
-    info['age'].toString().toUpperCase(),
-    info['job'].toUpperCase(),
-  ].join('-');
-
-  //- Hiển thị ra màn hình:  HOANG-23-IT.
-  print(str);
-
-  //- Thêm thông tin cho map: ‘email’: ‘abc@gmail.com’
-  info['email'] = 'abc@gmail.com';
-  print(info);
+  
 }
