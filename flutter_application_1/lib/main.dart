e69119afcabaae31fc00b49fb719d69cbd169b63
lamnv1992 <nguyenvanlamtrust@gmail.com>
2022-12-20 10:06:33 +0900

    Bài tập Flutter Buổi Buổi11
    Using Bottom Navigation Bar
    Stack
    Positioned
    ListView
    Row
    Column
    Image
    package:english_words/english_words.dart

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,149 +1,198 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
-import 'flutter_buoi_10.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  String get img => 'assets/animal.jpg';
+
+  get names => generateWordPairs().take(50).toList();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        primaryColor: Colors.green,
+        primarySwatch: Colors.green,
       ),
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Bài tập buổi 10'),
+          title: const Text('Messenger'),
         ),
-        body: _buildHomePage(),
+        body: _buildHomeScreen(),
+        bottomNavigationBar: _buildBottomNavigationBar(),
       ),
     );
   }
 
-  String get _txtSignUp => 'SING UP';
-
-  Widget _buildLabel(String text, double fSize) {
-    return Text(text, style: TextStyle(fontWeight: FontWeight.w500, fontSize: fSize));
-  }
-
-  Widget _buildHomePage() {
-    return Container(
-        padding: const EdgeInsets.all(40),
-        child: Column(
-          //crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            _buildTitle(),
-            _buildInput('Email', 'Enter your email'),
-            _buildSpacer(),
-            _buildInput('Password', 'Enter your password'),
-            _buildSpacer(),
-            _buildBtnSignUp(),
-            _buildSpacer(),
-            _buildLine(),
-            _buildSpacer(),
-            _buildSocialButtons(),
-            _buildBottomText(),
-          ],
-        ));
-  }
-
-  Widget _buildSpacer() {
-    return const SizedBox(height: 20);
-  }
-
-  Widget _buildTitle() {
-    return Container(
-      alignment: Alignment.centerLeft,
-      margin: const EdgeInsets.only(bottom: 30),
-      child: _buildLabel(_txtSignUp, 30),
-    );
-  }
-
-  Widget _buildInput(String text, String placeholder) {
+  Widget _buildHomeScreen() {
     return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        _buildLabel(text, 18),
-        TextField(
-          decoration: InputDecoration(
-            hintText: placeholder,
-            border: const OutlineInputBorder(),
-            contentPadding: const EdgeInsets.only(top: 0, bottom: 0, left: 10, right: 10),
-          ),
-        )
+        _buildSearchInput(),
+        _buildActivePeoples(),
+        _buildListMessage(),
       ],
     );
   }
 
-  Color get activeColor => const Color.fromARGB(159, 220, 15, 107);
-
-  Widget _buildBtnSignUp() {
+  Widget _buildSearchInput() {
     return Container(
-      width: double.infinity,
-      height: 50,
-      alignment: Alignment.center,
+      margin: const EdgeInsets.all(20),
       decoration: BoxDecoration(
-        color: activeColor,
+        color: const Color.fromARGB(255, 237, 234, 234),
+        border: Border.all(color: const Color.fromARGB(255, 237, 234, 234)),
         borderRadius: BorderRadius.circular(10),
       ),
-      child: Text(
-        _txtSignUp,
-        style: const TextStyle(
-          color: Colors.white,
-          fontSize: 17,
-          fontWeight: FontWeight.bold,
+      child: const TextField(
+        decoration: InputDecoration(
+          border: InputBorder.none,
+          hintText: 'Search...',
+          prefixIcon: Icon(Icons.search),
         ),
       ),
     );
   }
 
-  Widget _buildBottomText() {
-    return RichText(
-      text: const TextSpan(text: 'Already a user? ', style: TextStyle(fontSize: 18, color: Colors.black), children: [
-        TextSpan(text: 'LOGIN', style: TextStyle(decoration: TextDecoration.underline))
-      ]),
-    );
-  }
-
-  Widget _buildSocialButtons() {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 20),
-      child: Row(mainAxisSize: MainAxisSize.min, children: [
-        _buildBtnCircle('G', const Color.fromARGB(255, 200, 49, 38), 10),
-        _buildBtnCircle('f', const Color.fromARGB(255, 89, 107, 171), 10),
-        _buildBtnCircle('in', const Color.fromARGB(255, 58, 100, 184)),
-      ]),
-    );
-  }
-
-  Widget _buildBtnCircle(String text, Color color, [double mr = 0]) {
+  Widget _buildActivePeoples() {
     return Container(
-      width: 40,
-      height: 40,
-      margin: EdgeInsets.only(right: mr),
-      alignment: Alignment.center,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        border: Border.all(width: 3, color: color),
+      height: 120,
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: ListView.separated(
+        scrollDirection: Axis.horizontal,
+        itemCount: names.length,
+        itemBuilder: (context, index) {
+          return Column(
+            children: [
+              _buildActiveItemImage(),
+              const Divider(height: 10),
+              Text('${names[index]}'),
+            ],
+          );
+        },
+        separatorBuilder: (context, index) {
+          return const SizedBox(
+            width: 20,
+          );
+        },
       ),
-      child: Text(text, style: TextStyle(color: color, fontSize: 18, fontWeight: FontWeight.bold)),
     );
   }
 
-  Widget _buildLine() {
+  Widget _buildListMessage() {
+    return Expanded(
+      child: ListView.separated(
+        itemCount: names.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: _buildMessageItemImage(Random().nextInt(names.length) % 2 == 0 ? true : false),
+            title: _buildTitle(names[index].toString(), 20),
+            subtitle: Text([
+              'The view chat ended',
+              'Test message',
+            ][Random().nextInt(2)]),
+            trailing: _buildTrailingListTitle(),
+          );
+        },
+        separatorBuilder: (context, index) {
+          return const SizedBox(height: 10);
+        },
+      ),
+    );
+  }
+
+  Widget _buildImageItem([bool active = true]) {
     return Stack(
-      clipBehavior: Clip.antiAlias,
-      alignment: Alignment.center,
+      clipBehavior: Clip.none,
       children: [
-        const Divider(height: 60, color: Colors.grey),
-        Positioned(
-          top: 15,
-          child: Container(padding: const EdgeInsets.all(5), decoration: BoxDecoration(color: Colors.white, border: Border.all(color: Colors.grey), borderRadius: BorderRadius.circular(5)), child: const Text('OR')),
+        SizedBox(
+          width: 60,
+          height: 60,
+          child: CircleAvatar(
+            backgroundImage: AssetImage(img),
+          ),
         ),
+        !active
+            ? const Text('')
+            : Positioned(
+                bottom: 5,
+                right: -8,
+                child: Container(
+                  width: 20,
+                  height: 20,
+                  padding: const EdgeInsets.all(4),
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    shape: BoxShape.circle,
+                  ),
+                  child: Container(
+                    width: 8,
+                    height: 8,
+                    decoration: const BoxDecoration(
+                      color: Colors.green,
+                      shape: BoxShape.circle,
+                    ),
+                  ),
+                ),
+              ),
       ],
+    );
+  }
+
+  Widget _buildMessageItemImage([bool active = true]) {
+    return _buildImageItem(active);
+  }
+
+  Widget _buildActiveItemImage([bool active = true]) {
+    return Container(
+      padding: const EdgeInsets.all(2),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        shape: BoxShape.circle,
+        border: Border.all(color: Colors.grey),
+      ),
+      child: _buildImageItem(active),
+    );
+  }
+
+  Widget _buildTitle(String title, [double? fontSize]) {
+    return Text(
+      title,
+      style: TextStyle(
+        fontSize: fontSize,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+
+  Widget _buildBottomNavigationBar() {
+    return BottomNavigationBar(items: const [
+      BottomNavigationBarItem(
+        label: 'Chats',
+        icon: Icon(Icons.chat),
+      ),
+      BottomNavigationBarItem(
+        label: 'Story',
+        icon: Icon(Icons.camera),
+      ),
+    ]);
+  }
+
+  Widget _buildTrailingListTitle() {
+    return Container(
+      width: 20,
+      height: 20,
+      decoration: const BoxDecoration(
+        shape: BoxShape.circle,
+        color: Colors.grey,
+      ),
+      padding: const EdgeInsets.all(1),
+      child: const Center(
+        child: Icon(Icons.check, color: Colors.white, size: 15)
+      ),
     );
   }
 }
