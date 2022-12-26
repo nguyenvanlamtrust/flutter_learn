cadffa68db6888645bd3db0bfc50302605964e1a
nguyenvanlamtrust@gmail.com <nguyenvanlamtrust@gmail.com>
2022-12-26 09:39:52 +0900

    Chat App UI
    Update code.

---
+++ chat_app_ui/lib/model/chat_model.dart
@@ -0,0 +1,44 @@
+import 'package:chat_app_ui/utils/const_images.dart';
+
+class ChatModel {
+  final String avatar;
+  final String name;
+  final String chat;
+  final List<Message> messages;
+
+  ChatModel({required this.avatar, required this.name, required this.chat, required this.messages});
+}
+
+class Message {
+  final bool isSentByMe;
+  final String content;
+
+  Message({
+    required this.isSentByMe,
+    required this.content
+  });
+}
+
+final listChat = [
+  ChatModel(avatar: Images.man, name: 'Father', chat: 'What are you doing?', messages: [
+    Message(isSentByMe: false, content: 'Con dang lam gi day!aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'),
+    Message(isSentByMe: true, content: 'test.'),
+    Message(isSentByMe: false, content: 'test'),
+  ]),
+  ChatModel(avatar: Images.woman, name: 'Mother', chat: 'What are you doing?', messages: [
+    Message(isSentByMe: false, content: 'test test'),
+    Message(isSentByMe: true, content: 'test test'),
+  ]),
+  ChatModel(avatar: Images.woman, name: 'Sister', chat: 'What are you doing?', messages: [
+    Message(isSentByMe: false, content: 'test test'),
+    Message(isSentByMe: true, content: 'test test.'),
+  ]),
+  ChatModel(avatar: Images.woman, name: 'Wife', chat: 'What are you doing?', messages: [
+    Message(isSentByMe: false, content: 'test test'),
+    Message(isSentByMe: true, content: 'test test'),
+  ]),
+  ChatModel(avatar: Images.animal, name: 'Unknow', chat: 'What are you doing?', messages: [
+    Message(isSentByMe: false, content: 'Ban dang lam gi day'),
+    Message(isSentByMe: true, content: 'Ban la ai vay?'),
+  ]),
+];
---
+++ chat_app_ui/lib/screen/chat_screen.dart
@@ -0,0 +1,174 @@
+import 'package:flutter/material.dart';
+import 'package:chat_app_ui/model/chat_model.dart';
+import 'package:chat_app_ui/utils/const_images.dart';
+import 'package:chat_app_ui/utils/helper.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    
+    final ChatModel chat = ModalRoute.of(context)?.settings.arguments as ChatModel;
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Row(
+            children: [
+              Flexible(
+                flex: 7,
+                child: Row(
+                  children: [
+                    Helper.buildCircleAvatar(avatar: chat.avatar, transparentColor: true),
+                    const SizedBox(width: 10,),
+                    Text(chat.name),
+                  ]
+                )
+              ),
+              Flexible(
+                flex: 3,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end , 
+                  children: [
+                    GestureDetector(child: const Icon(Icons.call_sharp), onTap: () {}),
+                    const SizedBox(width: 10,),
+                    GestureDetector(child: const Icon(Icons.video_call_sharp), onTap: () {}),
+                  ]
+                )
+              ),
+            ],
+          ),
+        ),
+        body: _buildBody(chat));
+  }
+
+  Widget _buildBody(chat) {
+    if (chat.messages.isEmpty) {
+      return const Center(child: Text('No history chat.'));
+    }
+    return _buildListMessages(chat: chat);
+  }
+
+  Widget _buildListMessages({required ChatModel chat}) {
+    return SafeArea(
+      child: Column(
+        children: [
+          Expanded(
+            child: ListView.separated(
+                padding: const EdgeInsets.all(15),
+                itemBuilder: (context, index) {
+                  return _buildMessageItem(index: index, chat: chat);
+                },
+                separatorBuilder: (context, index) {
+                  return const SizedBox(height: 10);
+                },
+                itemCount: chat.messages.length),
+          ),
+          Row(
+            children: [
+              Flexible(flex: 4, child: _buildBottomLeftIcons()),
+              Flexible(
+                flex: 5,
+                child: _buildSendMessageInput(),
+              ),
+              Flexible(
+                flex: 1,
+                child: _buildBottomRightIcons(),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Icon _buildBottomRightIcons() => _buildIcon(Icons.thumb_up);
+
+  Row _buildBottomLeftIcons() {
+    return Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+      _buildIcon(Icons.send_sharp),
+      _buildIcon(Icons.local_see),
+      _buildIcon(Icons.image),
+      _buildIcon(Icons.mic),
+    ]);
+  }
+
+  Container _buildSendMessageInput() {
+    return Container(
+      height: 40,
+      padding: const EdgeInsets.symmetric(horizontal: 5),
+      decoration: BoxDecoration(
+        color: const Color(0xffefefef),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: TextField(
+        decoration: InputDecoration(
+          hintText: 'Aa',
+          suffixIcon: _buildIcon(Icons.emoji_emotions_sharp),
+          border: InputBorder.none,
+        ),
+      ),
+    );
+  }
+
+  Icon _buildIcon(IconData icon) {
+    return Icon(
+      icon,
+      color: Colors.blue,
+    );
+  }
+
+  Widget _buildMessageItem({required int index, required ChatModel chat}) {
+    Message msg = chat.messages[index];
+    return msg.isSentByMe ? messageSentByMe(msg: msg) : _buildReceivedMessage(msg: msg, avatar: chat.avatar);
+  }
+
+  Widget messageSentByMe({required Message msg}) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        Flexible(
+          flex: 1,
+          child: Container(),
+        ),
+        Flexible(
+            flex: 6,
+            child: Container(
+                padding: const EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Text(
+                  msg.content,
+                  style: const TextStyle(
+                    color: Colors.white,
+                  ),
+                ))),
+      ],
+    );
+  }
+
+  Widget _buildReceivedMessage({required Message msg, required String avatar}) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        Helper.buildCircleAvatar(avatar: avatar),
+        Flexible(
+            flex: 6,
+            child: Container(
+                padding: const EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                  color: const Color(0xffefefef),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Text(msg.content))),
+        Flexible(
+          flex: 1,
+          child: Container(),
+        ),
+      ],
+    );
+  }
+
+}
